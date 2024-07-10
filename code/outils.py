888026c6a44a5c2f8c00e79122d90eef534dd32d
abdulle-sabaf <abdulle-sabaf@users.noreply.github.com>
2024-07-10 17:09:22 +0800

    initial

---
+++ code/outils.py
@@ -0,0 +1,45 @@
+import json
+import codecs
+
+中文数字 = "〇一二三四五六七八九十"
+中转数 = {}
+数转中 = {}
+
+for i in range(11):
+    中转数[中文数字[i]] = i
+    数转中[i] = 中文数字[i]
+
+
+def keys(dct):
+    """return the keys of a dictionary as a list
+    """
+    return list(dct.keys())
+
+
+def load_cn_json(path):
+    """load Chinese content from a json file
+    """
+    with codecs.open(path, "r", encoding="utf-8") as f:
+        out = json.load(f)
+    return out
+
+
+def dump_cn_json(path, content):
+    """dump Chinese content to a json file
+    """
+    with codecs.open(path, "w", encoding="utf-8") as f:
+        json.dump(content, f, indent=4, ensure_ascii=False)
+
+def sort_dict_with(dic, key="grade"):
+    pox = {}
+    keys = []
+    for k, v in dic:
+        if v[key] not in pox:
+            pox[key] = []
+            keys.append(key)
+        pox[key].append((k, v))
+    
+    out = []
+    for key in sorted(keys):
+        out.extend(pox[key])
+    return out
